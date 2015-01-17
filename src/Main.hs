250c7757fd2761bc1a31da37adc82bd014e10138
Xavier Lange <xrlange@gmail.com>
2015-01-16 17:57:41 -0800

    Write to file

--- src/Main.hs
+++ src/Main.hs
@@ -1,25 +1,40 @@
 module Main (main) where
 
+import System.IO
 import UDPServer
-import qualified Data.Conduit as DC
+import qualified Data.Conduit as C
+import qualified Data.Conduit.List as CL
 import Data.Conduit.Network.UDP as DCU
 import Control.Monad.Trans (liftIO)
+import Data.ByteString.Char8 as BS
 
 import qualified Network.Socket as NS
 
 
 maxPacketSize = 5000
 
-producer :: NS.Socket -> DC.Source IO DCU.Message
-producer server = sourceSocket server maxPacketSize
+messageProducer :: NS.Socket -> C.Source IO DCU.Message
+messageProducer server = sourceSocket server maxPacketSize
 
-consumer :: DC.Sink DCU.Message IO ()
-consumer = DC.awaitForever $ liftIO . printMessageData
+printer :: C.Sink DCU.Message IO ()
+printer = C.awaitForever $ liftIO . printMessageData
   where
-    printMessageData message = putStrLn . show $ DCU.msgData message
+    printMessageData = BS.putStrLn . DCU.msgData
+
+msgToBS :: C.Conduit DCU.Message IO BS.ByteString
+msgToBS = CL.map DCU.msgData
+
+fileWriter :: Handle -> C.Sink BS.ByteString IO ()
+fileWriter handle = C.awaitForever $ liftIO . (writeMessageData handle)
+  where
+    writeMessageData handle = BS.hPutStrLn handle
 
 main :: IO ()
 main = do
   udpServer <- newSyslogListener
-  let udpMessages = producer udpServer
-  udpMessages DC.$$ consumer
+  let udpMessages = messageProducer udpServer
+
+  logFile <- openFile "syslog.log" ReadMode
+  let logWriter = fileWriter logFile
+
+  udpMessages C.$= msgToBS C.$$ logWriter
